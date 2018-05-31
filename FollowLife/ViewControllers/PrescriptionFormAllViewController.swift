4203e8329a1fc05d83b15a7b44f36e9e4de5e26f
Hugo Andres <hugoand@MacBook-Air-de-Hugo.local>
2018-05-30 20:26:48 -0500

    Constraint errors fixed and two classes'name changed

--- FollowLife/ViewControllers/PrescriptionFormAllViewController.swift
+++ FollowLife/ViewControllers/PrescriptionFormAllViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class prescriptionFormAllViewController: UIViewController {
+class PrescriptionFormAllViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- FollowLife/ViewControllers/PrescriptionFormMedViewController.swift
+++ FollowLife/ViewControllers/PrescriptionFormMedViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class prescriptionFormMedViewController: UIViewController {
+class PrescriptionFormMedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
